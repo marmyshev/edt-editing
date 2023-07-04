c817338e55e588442bd07815dd143f3e072cc7e0
Dmitriy Marmyshev <marmyshev@gmail.com>
2023-07-04 18:49:22 +0000

    Added Project editor section to create new settings or open exiting

--- org.mard.dt.editing.ui/src/org/mard/dt/editing/ui/decorators/DisableEditing.java
+++ org.mard.dt.editing.ui/src/org/mard/dt/editing/ui/decorators/DisableEditing.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (C) 2021, Dmitriy Marmyshev and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Dmitriy Marmyshev - initial API and implementation
+ *******************************************************************************/
 package org.mard.dt.editing.ui.decorators;
 
 import java.net.URL;
